e9e8e10a7f40010f676b4ad2110aa4c80a5b4b5b
Luis Santos <luisfcs@hotmail.co.uk>
2022-04-10 21:02:30 +0100

    memory data assigned
    using ghw to assign memory details.

--- mem/mem.go
+++ mem/mem.go
@@ -1,5 +1,11 @@
 package memory
 
+import (
+	"fmt"
+
+	"github.com/jaypipes/ghw"
+)
+
 type Memory struct {
 	TotalPhysicalBytes int64       `json:"total_physical_bytes"`
 	TotalUsableBytes   int64       `json:"total_usable_bytes"`
@@ -13,4 +19,28 @@ type Modules struct {
 	SerialNumber string `json:"serial_number"`
 	SizeBytes    int64  `json:"size_bytes"`
 	Vendor       string `json:"vendor"`
+}
+
+func (m *Memory) Json() {
+
+	mem, err := ghw.Memory()
+	if err != nil {
+		fmt.Printf("Error getting memory info: %v", err)
+	}
+
+	m.TotalPhysicalBytes = mem.TotalPhysicalBytes
+	m.TotalUsableBytes = mem.TotalUsableBytes
+	m.SupportedPageSizes = mem.SupportedPageSizes
+
+	md := Modules{}
+	for _, y := range mem.Modules {
+		//fmt.Printf("%d -- %v\n", x, y.Vendor)
+		md.Label = y.Label
+		md.Location = y.Location
+		md.Vendor = y.Vendor
+		md.SerialNumber = y.SerialNumber
+		md.SizeBytes = y.SizeBytes
+
+		m.Modules = append(m.Modules, md)
+	}
 }
