3af645a23d1ee656420f8546db705b6291358c57
Luis Santos <luisfcs@hotmail.co.uk>
2022-04-12 10:39:45 +0100

    bios info
    added bios information

---
+++ bios/bios.go
@@ -0,0 +1,25 @@
+package bios
+
+import (
+	"fmt"
+
+	"github.com/jaypipes/ghw"
+)
+
+type Bios struct {
+	Vendor  string `json:"vendor"`
+	Version string `json:"version"`
+	Date    string `json:"date"`
+}
+
+func (*Bios) Get(b *Bios) {
+	bi, err := ghw.BIOS()
+	if err != nil {
+		fmt.Printf("Error getting cpu info: %v", err)
+	}
+
+	b.Vendor = bi.Vendor
+	b.Version = bi.Version
+	b.Date = bi.Date
+
+}
