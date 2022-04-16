999da860e86d7559e69e672bc7f33a34287b121f
Luis Santos <luisfcs@hotmail.co.uk>
2022-04-16 22:53:25 +0100

    baseboard information
    added baseboard info

--- baseboard/baseboard.go
+++ baseboard/baseboard.go
@@ -1,9 +1,29 @@
 package baseboard
 
+import (
+	"fmt"
+
+	"github.com/jaypipes/ghw"
+)
+
 type Baseboard struct {
 	AssetTag     string `json:"asset_tag"`
 	SerialNumber string `json:"serial_number"`
 	Vendor       string `json:"vendor"`
 	Version      string `json:"version"`
 	Product      string `json:"product"`
+}
+
+func (*Baseboard) Get(b *Baseboard) {
+
+	bb, err := ghw.Baseboard()
+	if err != nil {
+		fmt.Printf("Error getting net info: %v", err)
+	}
+
+	b.AssetTag = bb.AssetTag
+	b.SerialNumber = bb.SerialNumber
+	b.Vendor = bb.Vendor
+	b.Version = bb.Version
+	b.Product = bb.Product
 }
