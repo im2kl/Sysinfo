08949898a35acad411490b174a70cc78f8c5bb3a
Luis Santos <luisfcs@hotmail.co.uk>
2022-04-16 22:29:51 +0100

    chassis information
    added chassis information

--- chassis/chassis.go
+++ chassis/chassis.go
@@ -1,5 +1,11 @@
 package chassis
 
+import (
+	"fmt"
+
+	"github.com/jaypipes/ghw"
+)
+
 type Chassis struct {
 	AssetTag        string `json:"asset_tag"`
 	SerialNumber    string `json:"serial_number"`
@@ -7,4 +13,20 @@ type Chassis struct {
 	TypeDescription string `json:"type_description"`
 	Vendor          string `json:"vendor"`
 	Version         string `json:"version"`
+}
+
+func (*Chassis) Get(c *Chassis) {
+
+	cha, err := ghw.Chassis()
+	if err != nil {
+		fmt.Printf("Error getting Chassis info: %v", err)
+	}
+
+	c.AssetTag = cha.AssetTag
+	c.SerialNumber = cha.SerialNumber
+	c.Type = cha.Type
+	c.TypeDescription = cha.TypeDescription
+	c.Vendor = cha.Vendor
+	c.Version = cha.Version
+
 }
