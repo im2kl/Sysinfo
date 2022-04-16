ac93b278c3303a7144edb51efedd744e6cc1b0d1
Luis Santos <luisfcs@hotmail.co.uk>
2022-04-16 22:42:53 +0100

    added product
    populating product information

--- product/product.go
+++ product/product.go
@@ -1,5 +1,11 @@
 package product
 
+import (
+	"fmt"
+
+	"github.com/jaypipes/ghw"
+)
+
 type Product struct {
 	Family       string `json:"family"`
 	Name         string `json:"name"`
@@ -8,4 +14,21 @@ type Product struct {
 	UUID         string `json:"uuid"`
 	Sku          string `json:"sku"`
 	Version      string `json:"version"`
+}
+
+func (*Product) Get(p *Product) {
+
+	pro, err := ghw.Product()
+	if err != nil {
+		fmt.Printf("Error getting net info: %v", err)
+	}
+
+	p.Family = pro.Family
+	p.Name = pro.Name
+	p.Vendor = pro.Vendor
+	p.SerialNumber = pro.SerialNumber
+	p.UUID = pro.UUID
+	p.Sku = pro.SKU
+	p.Version = pro.Version
+
 }
