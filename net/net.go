553bd3b092ae0295249d9e0bb9f8ae5842d9c7ef
Luis Santos <luisfcs@hotmail.co.uk>
2022-04-16 22:11:20 +0100

    network populate
    added network.get can now get nic information.

--- net/net.go
+++ net/net.go
@@ -1,5 +1,11 @@
 package net
 
+import (
+	"fmt"
+
+	"github.com/jaypipes/ghw"
+)
+
 type Network struct {
 	Nics []Nics `json:"nics"`
 }
@@ -9,4 +15,23 @@ type Nics struct {
 	MacAddress string `json:"mac_address"`
 	IsVirtual  bool   `json:"is_virtual"`
 	// Capabilities []interface{} `json:"capabilities"`
+}
+
+func (*Network) Get(n *Network) {
+
+	net, err := ghw.Network()
+	if err != nil {
+		fmt.Printf("Error getting net info: %v", err)
+	}
+
+	nc := Nics{}
+
+	for _, y := range net.NICs {
+
+		nc.Name = y.Name
+		nc.MacAddress = y.MacAddress
+		nc.IsVirtual = y.IsVirtual
+
+		n.Nics = append(n.Nics, nc)
+	}
 }
